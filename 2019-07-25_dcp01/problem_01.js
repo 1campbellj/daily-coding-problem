b89bdbdddf0a1aa0e87391368ca78fd12957c984
1campbellj <1campbellj@gmail.com>
2019-07-25 20:29:47 -0500

    Fix big in problem 1 and write more tests

--- 2019-07-25_dcp01/problem_01.js
+++ 2019-07-25_dcp01/problem_01.js
@@ -7,14 +7,15 @@ var run = function(arr, sum) {
   for (i = 0; i < arr.length; i++) {
     // if the cache is true for i, i + x = sum
     // where x is some previous element in the array
-    if (cache[i]) {
-      return true;
-    }
 
     // no combination found, store current difference
     // for easy lookup later
     m = sum - arr[i];
     cache[m] = true;
+
+    if (cache[arr[i]]) {
+      return true;
+    }
   }
 
   return false;
--- 2019-07-25_dcp01/test.js
+++ 2019-07-25_dcp01/test.js
@@ -6,3 +6,10 @@ var k = 17;
 
 console.log('Test given use case');
 assert(p1.run(list, k));
+
+console.log('Test a bunch of other cases');
+assert(p1.run(list, 10));
+assert(!p1.run(list, 9));
+assert(p1.run(list, 22));
+assert(!p1.run(list, 333));
+assert(!p1.run(list, -5));
